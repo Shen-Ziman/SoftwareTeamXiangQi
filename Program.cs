042a619119f24e5f78a3caebe2ef1c874be0083c
Shen-Ziman <765353219@qq.com>
2021-11-01 10:27:24 +0800

    Initiated CSharp console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareTeamXiangQi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
