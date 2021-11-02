888168d78062d70b709e2e53a746852a31975551
Shen-Ziman <765353219@qq.com>
2021-11-02 23:41:36 +0800

    add ChessBoard

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+// using System.Collections;
+// using System.Data;
 namespace SoftwareTeamXiangQi
 {
     //Try and try
@@ -6,7 +8,8 @@ namespace SoftwareTeamXiangQi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");                 
+            Console.WriteLine("Hello World!");    
+                          
         }
     }
 }
